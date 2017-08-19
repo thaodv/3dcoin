25b79647e2af57815ad7b81380c1b4ae5852d7a9
RezigZine\Blockchain LLC <admin@districts.io>
2017-08-19 03:22:55 +0100

    algo test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -117,7 +117,7 @@ public:
         pchMessageStart[2] = 0x6b;
         pchMessageStart[3] = 0xbd;
         vAlertPubKey = ParseHex("048240a8748a80a286b270ba126705ced4f2ce5a7847b3610ea3c06513150dade2a8512ed5ea86320824683fc0818f0ac019214973e677acd1244f6d0571fc5103");
-        nDefaultPort = 9999;
+        nDefaultPort = 16016;
         nMaxTipAge = 6 * 60 * 60; // ~144 blocks behind -> 2 x fork detection time, was 24 * 60 * 60 in bitcoin
         nPruneAfterHeight = 100000;
 
@@ -236,7 +236,7 @@ public:
         pchMessageStart[2] = 0xca;
         pchMessageStart[3] = 0xff;
         vAlertPubKey = ParseHex("04517d8a699cb43d3938d7b24faaff7cda448ca4ea267723ba614784de661949bf632d6304316b244646dea079735b9a6fc4af804efb4752075b9fe2245e14e412");
-        nDefaultPort = 19999;
+        nDefaultPort = 17017;
         nMaxTipAge = 0x7fffffff; // allow mining on top of old blocks for testnet
         nPruneAfterHeight = 1000;
 
@@ -341,7 +341,7 @@ public:
         pchMessageStart[2] = 0xb7;
         pchMessageStart[3] = 0xdc;
         nMaxTipAge = 6 * 60 * 60; // ~144 blocks behind -> 2 x fork detection time, was 24 * 60 * 60 in bitcoin
-        nDefaultPort = 19994;
+        nDefaultPort = 15015;
         nPruneAfterHeight = 1000;
 
         genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
--- src/hash.h
+++ src/hash.h
@@ -37,35 +37,30 @@ typedef uint256 ChainCode;
 #endif
 
 GLOBAL sph_blake512_context     z_blake;
-GLOBAL sph_bmw512_context       z_bmw;
+//GLOBAL sph_bmw512_context       z_bmw;
 GLOBAL sph_groestl512_context   z_groestl;
-GLOBAL sph_jh512_context        z_jh;
+//GLOBAL sph_jh512_context        z_jh;
 GLOBAL sph_keccak512_context    z_keccak;
 GLOBAL sph_skein512_context     z_skein;
-GLOBAL sph_luffa512_context     z_luffa;
+//GLOBAL sph_luffa512_context     z_luffa;
 GLOBAL sph_cubehash512_context  z_cubehash;
-GLOBAL sph_shavite512_context   z_shavite;
-GLOBAL sph_simd512_context      z_simd;
-GLOBAL sph_echo512_context      z_echo;
+//GLOBAL sph_shavite512_context   z_shavite;
+//GLOBAL sph_simd512_context      z_simd;
+//GLOBAL sph_echo512_context      z_echo;
 
 #define fillz() do { \
     sph_blake512_init(&z_blake); \
-    sph_bmw512_init(&z_bmw); \
     sph_groestl512_init(&z_groestl); \
-    sph_jh512_init(&z_jh); \
     sph_keccak512_init(&z_keccak); \
     sph_skein512_init(&z_skein); \
-    sph_luffa512_init(&z_luffa); \
     sph_cubehash512_init(&z_cubehash); \
-    sph_shavite512_init(&z_shavite); \
-    sph_simd512_init(&z_simd); \
-    sph_echo512_init(&z_echo); \
+    
 } while (0)
 
 #define ZBLAKE (memcpy(&ctx_blake, &z_blake, sizeof(z_blake)))
-#define ZBMW (memcpy(&ctx_bmw, &z_bmw, sizeof(z_bmw)))
+//#define ZBMW (memcpy(&ctx_bmw, &z_bmw, sizeof(z_bmw)))
 #define ZGROESTL (memcpy(&ctx_groestl, &z_groestl, sizeof(z_groestl)))
-#define ZJH (memcpy(&ctx_jh, &z_jh, sizeof(z_jh)))
+//#define ZJH (memcpy(&ctx_jh, &z_jh, sizeof(z_jh)))
 #define ZKECCAK (memcpy(&ctx_keccak, &z_keccak, sizeof(z_keccak)))
 #define ZSKEIN (memcpy(&ctx_skein, &z_skein, sizeof(z_skein)))
 
@@ -284,16 +279,16 @@ inline uint256 HashX11(const T1 pbegin, const T1 pend)
 
 {
     sph_blake512_context     ctx_blake;
-    sph_bmw512_context       ctx_bmw;
+    //sph_bmw512_context       ctx_bmw;
     sph_groestl512_context   ctx_groestl;
-    sph_jh512_context        ctx_jh;
+    //sph_jh512_context        ctx_jh;
     sph_keccak512_context    ctx_keccak;
     sph_skein512_context     ctx_skein;
-    sph_luffa512_context     ctx_luffa;
+    //sph_luffa512_context     ctx_luffa;
     sph_cubehash512_context  ctx_cubehash;
-    sph_shavite512_context   ctx_shavite;
-    sph_simd512_context      ctx_simd;
-    sph_echo512_context      ctx_echo;
+    //sph_shavite512_context   ctx_shavite;
+    //sph_simd512_context      ctx_simd;
+    //sph_echo512_context      ctx_echo;
     static unsigned char pblank[1];
 
     uint512 hash[11];
@@ -302,9 +297,9 @@ inline uint256 HashX11(const T1 pbegin, const T1 pend)
     sph_blake512 (&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
     sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
 
-    sph_bmw512_init(&ctx_bmw);
+    /*sph_bmw512_init(&ctx_bmw);
     sph_bmw512 (&ctx_bmw, static_cast<const void*>(&hash[0]), 64);
-    sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[1]));
+    sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[1]));*/
 
     sph_groestl512_init(&ctx_groestl);
     sph_groestl512 (&ctx_groestl, static_cast<const void*>(&hash[1]), 64);
@@ -314,23 +309,23 @@ inline uint256 HashX11(const T1 pbegin, const T1 pend)
     sph_skein512 (&ctx_skein, static_cast<const void*>(&hash[2]), 64);
     sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[3]));
 
-    sph_jh512_init(&ctx_jh);
+    /*sph_jh512_init(&ctx_jh);
     sph_jh512 (&ctx_jh, static_cast<const void*>(&hash[3]), 64);
-    sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[4]));
+    sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[4]));*/
 
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512 (&ctx_keccak, static_cast<const void*>(&hash[4]), 64);
     sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[5]));
 
-    sph_luffa512_init(&ctx_luffa);
+    /*sph_luffa512_init(&ctx_luffa);
     sph_luffa512 (&ctx_luffa, static_cast<void*>(&hash[5]), 64);
-    sph_luffa512_close(&ctx_luffa, static_cast<void*>(&hash[6]));
+    sph_luffa512_close(&ctx_luffa, static_cast<void*>(&hash[6]));*/
 
     sph_cubehash512_init(&ctx_cubehash);
     sph_cubehash512 (&ctx_cubehash, static_cast<const void*>(&hash[6]), 64);
     sph_cubehash512_close(&ctx_cubehash, static_cast<void*>(&hash[7]));
 
-    sph_shavite512_init(&ctx_shavite);
+    /*sph_shavite512_init(&ctx_shavite);
     sph_shavite512(&ctx_shavite, static_cast<const void*>(&hash[7]), 64);
     sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[8]));
 
@@ -340,7 +335,7 @@ inline uint256 HashX11(const T1 pbegin, const T1 pend)
 
     sph_echo512_init(&ctx_echo);
     sph_echo512 (&ctx_echo, static_cast<const void*>(&hash[9]), 64);
-    sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[10]));
+    sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[10]));*/
 
     return hash[10].trim256();
 }
