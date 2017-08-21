467a08eecd17eba361a8f4d94489b79ce06a9d88
RezigZine\Blockchain LLC <admin@districts.io>
2017-08-21 18:02:00 +0100

    disable rpcmining on mainnet

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -134,9 +134,9 @@ UniValue generate(const UniValue& params, bool fHelp)
             "\nGenerate 11 blocks\n"
             + HelpExampleCli("generate", "11")
         );
-	//RPC mining mainnet(generate mainnet)
-    /*if (!Params().MineBlocksOnDemand())
-        throw JSONRPCError(RPC_METHOD_NOT_FOUND, "This method can only be used on regtest");*/
+	
+    if (!Params().MineBlocksOnDemand())
+        throw JSONRPCError(RPC_METHOD_NOT_FOUND, "This method can only be used on regtest");
 
     int nHeightStart = 0;
     int nHeightEnd = 0;
