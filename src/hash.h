2fe977da1f1c9aefe4281a77f0ff9f2a533c86cb
RezigZine\Blockchain LLC <admin@districts.io>
2017-08-26 13:06:19 +0100

    groestl test

--- src/hash.h
+++ src/hash.h
@@ -267,25 +267,27 @@ template<typename T1>
 inline uint256 Cassiopeia(const T1 pbegin, const T1 pend)
 
 {
-    sph_blake512_context     ctx_blake;
+    /*sph_blake512_context     ctx_blake;*/
     sph_groestl512_context   ctx_groestl;
-    sph_jh512_context        ctx_jh;
+    /*sph_jh512_context        ctx_jh;
     sph_keccak512_context    ctx_keccak;
-    sph_skein512_context     ctx_skein;
+    sph_skein512_context     ctx_skein;*/
 
     static unsigned char pblank[1];
 
-    uint512 hash[5];
+    uint512 hash[1];
 
-    sph_blake512_init(&ctx_blake);
+    /*sph_blake512_init(&ctx_blake);
     sph_blake512 (&ctx_blake, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
-    sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));
+    sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[0]));*/
 
     sph_groestl512_init(&ctx_groestl);
-    sph_groestl512 (&ctx_groestl, static_cast<const void*>(&hash[0]), 64);
-    sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[1]));
+    sph_groestl512 (&ctx_groestl, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]), 64);
+    sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[0]));
 
-    sph_skein512_init(&ctx_skein);
+
+
+    /*sph_skein512_init(&ctx_skein);
     sph_skein512 (&ctx_skein, static_cast<const void*>(&hash[1]), 64);
     sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[2]));
 
@@ -295,10 +297,10 @@ inline uint256 Cassiopeia(const T1 pbegin, const T1 pend)
 
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512 (&ctx_keccak, static_cast<const void*>(&hash[3]), 64);
-    sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[4]));
+    sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[4]));*/
 
 
-    return hash[4].trim256();
+    return hash[1].trim256();
 }
 
 #endif // BITCOIN_HASH_H
